6e9fff3fea6518db0ae3819cb9779f5b4b6e6d34
Pedro Gomes <aluno21279@ipt.pt>
2021-05-05 12:33:50 +0100

    adição de atributo tipo 'decimal' ao veterinario

--- CriadoresCaes/CriadoresCaes/Migrations/20210505105806_HonorariosVet01.Designer.cs
+++ CriadoresCaes/CriadoresCaes/Migrations/20210505105806_HonorariosVet01.Designer.cs
@@ -4,14 +4,16 @@ using CriadoresCaes.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CriadoresCaes.Migrations
 {
     [DbContext(typeof(CriadoresCaesDB))]
-    partial class CriadoresCaesDBModelSnapshot : ModelSnapshot
+    [Migration("20210505105806_HonorariosVet01")]
+    partial class HonorariosVet01
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
@@ -609,7 +611,7 @@ namespace CriadoresCaes.Migrations
                     b.Property<string>("Id")
                         .HasColumnType("nvarchar(450)");
 
-                    b.Property<decimal?>("Honorarios")
+                    b.Property<decimal>("Honorarios")
                         .HasColumnType("decimal(18,2)");
 
                     b.Property<string>("Nome")
--- CriadoresCaes/CriadoresCaes/Models/Veterinarios.cs
+++ CriadoresCaes/CriadoresCaes/Models/Veterinarios.cs
@@ -18,11 +18,33 @@ namespace CriadoresCaes.Models
         {
             ListaCaesTratadosPeloVeterinario = new HashSet<Caes>();
         }
+
+        /// <summary>
+        /// Identificador do Veterinario
+        /// </summary>
         [Key]
         [DatabaseGenerated(DatabaseGeneratedOption.None)]
         public string Id { get; set; }
 
+        /// <summary>
+        /// nome do Veterinário
+        /// </summary>
         public string Nome { get; set; }
+
+        /// <summary>
+        /// Montante cobrado pelo Veterinário numa consulta
+        /// </summary>
+        public decimal? Honorarios { get; set; }
+
+        /// <summary>
+        /// atributo auxiliar para receber o valor dos honorarios do Veterinario
+        /// </summary>
+        [NotMapped] //este atributo não vai ser adicionado à BD
+        [Required]
+        [Display(Name ="Honorários")]
+        [RegularExpression("[0..9]+[.,]?[0-9]{0,2}")] //formata a texbox para só aceitar valores decimais
+       // [RegularExpression("[0..9]+[.,][0-9]{2}?")]   ------> outra alternativa
+        public string HonorarioAux { get; set; }
 
         //***************************************************************************
         //identificar os cães que são tratados pelo Veterinário
